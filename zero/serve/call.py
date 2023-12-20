7842995a44fb98701f4e59b031740fa261d07282
熊超 <chao.xiong@9m-tec.com>
2023-12-20 16:20:13 +0800

    feat: function register

--- zero/serve/call.py
+++ zero/serve/call.py
@@ -1,13 +1,5 @@
-from functools import wraps
-
-
 def rpc(name):
-    def wrapper(func):
-        @wraps(func)
-        def inner(self, request, context):
-            setattr(self, name, func)
-            return func(self, request, context)
-
-        return inner
+    def wrapper(f):
+        return f
 
     return wrapper
