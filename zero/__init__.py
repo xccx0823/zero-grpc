760bf94e85d5251df1f000f9436d91c8f33dd2b1
熊超 <cheerxiong0823@163.com>
2023-12-21 21:42:37 +0800

    feat: add view

--- zero/__init__.py
+++ zero/__init__.py
@@ -1,1 +1,2 @@
 from zero.serve.app import Zero, current
+from zero.serve.view import View
---
+++ zero/serve/view.py
@@ -0,0 +1,9 @@
+from typing import Any
+
+
+class View:
+    """Provide a method prompt to eliminate the prompt of not
+    inheriting this property when using the editor. If you can
+    tolerate it, you can also choose not to inherit this type.
+    """
+    pb2: Any
