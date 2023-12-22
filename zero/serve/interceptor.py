36160035de5c01108bd9ca91488d96533d03f745
熊超 <cheerxiong0823@163.com>
2023-12-22 19:17:38 +0800

    fix:rename

--- zero/serve/interceptor.py
+++ zero/serve/interceptor.py
@@ -9,11 +9,11 @@ def setup_logger_interceptor(app):
     Request log blocker in debug mode.
     """
 
-    class DebubRequestLogInterceptor(grpc.ServerInterceptor):
+    class DebugRequestLogInterceptor(grpc.ServerInterceptor):
         def intercept_service(self, continuation, handler_call_details):
             begin_time = time.time()
             response = continuation(handler_call_details)
             app.log.info(f"{datetime.now()} INFO {handler_call_details.method} {time.time() - begin_time}s")
             return response
 
-    return DebubRequestLogInterceptor
+    return DebugRequestLogInterceptor
