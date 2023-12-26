d6dec9b55e15033c18d739b7114ef8a46206e873
熊超 <chao.xiong@9m-tec.com>
2023-12-26 17:46:14 +0800

    fix: scheduler api

---
+++ zero/pkg/scheduler/api.py
@@ -0,0 +1,28 @@
+class SchedulerServicer:
+
+    def get_scheduler_info(self, request, context):
+        pass
+
+    def add_job(self, request, context):
+        pass
+
+    def get_job(self, request, context):
+        pass
+
+    def get_jobs(self, request, context):
+        pass
+
+    def delete_job(self, request, context):
+        pass
+
+    def update_job(self, request, context):
+        pass
+
+    def pause_job(self, request, context):
+        pass
+
+    def resume_job(self, request, context):
+        pass
+
+    def run_job(self, request, context):
+        pass
